098e72d5e78020e43e0251a9a6027cc285c848ef
esraasay <esraa.krobeen@gmail.com>
2023-11-16 02:06:19 +0300

    execute_builtins

---
+++ execute_builtins.c
@@ -0,0 +1,21 @@
+#include "main.h"
+void execute_builtins(char **args)
+{
+	int i;
+
+	if (args)
+	{
+		for (i = 0; i < num_builtins(); i++)
+		{
+			if (strcmp(args[0], builtins[i].name) == 0)
+			{
+				builtins[i].func(args);
+				return;
+			}
+		}
+	}
+	else
+	{
+		perror("Error command not found");
+	}
+}
